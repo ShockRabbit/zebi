2cf630e867b883a0b21b8254112f3fad0e47a43f
ShockRabbit <shockrabbit17@gmail.com>
2020-06-24 17:41:57 +0900

    installer/pyenv.sh 정상 동작 확인. 그러나 기본 설정해주는 코드가 추가되어야 함

--- installer/pyenv.sh
+++ installer/pyenv.sh
@@ -8,24 +8,35 @@ function install_process_pyenv() {
     brew install pyenv-virtualenv
 
     # pyenv_env_infos.json file 읽어들여 environments 생성 및 packages 설치
-    local versions=`cat $config_path | jq -r ".pyenv | .versions[].version"`
     local global=`cat $config_path | jq -r ".pyenv | .global"`
+    local versions=`cat $config_path | jq -r ".pyenv | .versions[].version"`
     for v in $versions; do
-        echo_title "create pyenv python version ${v}"
+        echo "-------------------------------------------------"
+        echo "create pyenv python version ${v}"
+        echo "-------------------------------------------------"
         pyenv install $v
+        
         local names=`cat $config_path | jq -r ".pyenv | .versions[] | select(.version==\"${v}\") | .envs[].name"`
         for n in $names; do
-            echo_title "create env ${n} in python version ${v}"
+            echo "-------------------------------------------------"
+            echo "create env ${n} in python version ${v}"
+            echo "-------------------------------------------------"
             pyenv virtualenv $v $n
             source activate $n
             
-            local pkgs=`cat $config_path | jq -r ".pyenv | .versions[] | select(.version==\"${v}\") | .envs[] | select(.name==\"${n}\") | .packages"`
-            echo_title "install python packages by ${pkgs}"
-            pip install -r $pkgs
+            local packages=`cat $config_path | jq -r ".pyenv | .versions[] | select(.version==\"${v}\") | .envs[] | select(.name==\"${n}\") | .packages[]"`
+            for p in $packages; do
+                echo "install ${p}"
+                pip install $p
+            done
         done
     done
     # check and set default env
-    echo_title "set default version : ${global}"
+    echo "-------------------------------------------------"
+    echo "set default version : ${global}"
+    echo "-------------------------------------------------"
     pyenv global $global
     source activate $global # brew install 은 항상 default env 에서 이뤄져야 하므로 activate 한다.
 }
+
+#install_process_pyenv ../config.json
