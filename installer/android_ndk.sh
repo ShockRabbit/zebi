35ea6f740300d45fb311cf0e58ca0423c0370973
ShockRabbit <shockrabbit17@gmail.com>
2020-06-26 21:17:11 +0900

    android_sdk, ndk 수정 및 테스트 완료

--- installer/android_ndk.sh
+++ installer/android_ndk.sh
@@ -6,14 +6,21 @@ function install_process_android_ndk() {
     local location_str=`cat $config_path | jq -r ".android_ndk | .location"`
     local location=$(eval echo $location_str)
     local download_url=`cat $config_path | jq -r ".android_ndk | .download_url"`
+    local temp_path=./temp_for_install
     local ndk_temp_path=$temp_path/android_ndk
     local ndk_zip_path=$ndk_temp_path/ndk.zip
     if [ ! -d "$ndk_temp_path" ]; then
-        mkdir $ndk_temp_path
+        mkdir -p $ndk_temp_path
     fi
     if [ ! -d "$location" ]; then
-        mkdir $location
+        mkdir -p $location
     fi
+
+    echo "-----------------------------------------------------"
+    echo "Download Android NDK from ${download_url} to ${location}"
+    echo "-----------------------------------------------------"
     wget -O $ndk_zip_path $download_url
     unzip $ndk_zip_path -d $location
 }
+
+install_process_android_ndk ../config.json
--- installer/android_sdk.sh
+++ installer/android_sdk.sh
@@ -8,6 +8,7 @@ function install_process_android_sdk() {
         brew cask install adoptopenjdk8
     fi
 
+    local temp_path=./temp_for_install
     local sdktools_temp_path=$temp_path/android_sdk_tools
     local sdktools_zip_path=$sdktools_temp_path/sdk-tools.zip
     local location_str=`cat $config_path | jq -r ".android_sdk | .location"`
@@ -18,13 +19,19 @@ function install_process_android_sdk() {
 
     local sdkmanager=$location/tools/bin/sdkmanager
     if [ ! -d "$sdktools_temp_path" ]; then
-        mkdir $sdktools_temp_path
+        mkdir -p $sdktools_temp_path
     fi
     if [ ! -d "$location" ]; then
-        mkdir $location
+        mkdir -p $location
     fi
+
+    echo "-----------------------------------------------------"
+    echo "Download Android SDK from ${download_url} to ${location}"
+    echo "-----------------------------------------------------"
     wget -O $sdktools_zip_path $download_url
     unzip $sdktools_zip_path -d $location
     yes | $sdkmanager --licenses
     $sdkmanager "platform-tools" "platforms;android-$sdk_api" "build-tools;$build_tools"
 }
+
+#install_process_android_sdk ../config.json
