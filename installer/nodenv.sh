4c52cf38f83ec5de81df60e707fe1df2f2f2b8ba
ShockRabbit <shockrabbit17@gmail.com>
2020-06-26 16:09:51 +0900

    nodenv 수정 및 테스트 완료

--- installer/nodenv.sh
+++ installer/nodenv.sh
@@ -3,11 +3,21 @@
 function install_process_nodenv() {
     local config_path=$1
     
-    brew install nodenv
-    nodenv init
+    brew install nodenv node-build
+
+    ###################################################################################
+    # Setup
+    #
+    # add nodenv init to shell
+    # FIXME 추후 shell type 에 따라서 수정할 설정 파일을 바꿔야할 듯..
+    echo 'if command -v nodenv 1>/dev/null 2>&1; then\n  eval "$(nodenv init -)"\nfi' >> ~/.bash_profile
+    # Restart shell
+    #exec "$SHELL"
+    source ~/.bash_profile
+    ###################################################################################
 
     local versions=`cat $config_path | jq -r ".nodenv | .versions[].version"`
-    local global=`cat $config_path | jq -r ".nodenv | global"`
+    local global=`cat $config_path | jq -r ".nodenv | .global"`
 
     for v in $versions; do
         nodenv install $v
@@ -20,3 +30,5 @@ function install_process_nodenv() {
     nodenv global $global
     nodenv local $global
 }
+
+# install_process_nodenv ../config.json
