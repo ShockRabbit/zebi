74502470d81b0c9b968037807a2a5fcab5bb00e3
ShockRabbit <shockrabbit17@gmail.com>
2020-06-26 16:02:17 +0900

    rbenv 수정 및 테스트 완료

--- installer/pyenv.sh
+++ installer/pyenv.sh
@@ -35,7 +35,7 @@ function install_process_pyenv() {
             echo "create env ${n} in python version ${v}"
             echo "-------------------------------------------------"
             pyenv virtualenv $v $n
-            source activate $n
+            pyenv activate $n
             
             local packages=`cat $config_path | jq -r ".pyenv | .versions[] | select(.version==\"${v}\") | .envs[] | select(.name==\"${n}\") | .packages[]"`
             for p in $packages; do
@@ -49,7 +49,7 @@ function install_process_pyenv() {
     echo "set default version : ${global}"
     echo "-------------------------------------------------"
     pyenv global $global
-    source activate $global # brew install 은 항상 default env 에서 이뤄져야 하므로 activate 한다.
+    pyenv activate $global # brew install 은 항상 default env 에서 이뤄져야 하므로 activate 한다.
 }
 
 #install_process_pyenv ../config.json
--- installer/rbenv.sh
+++ installer/rbenv.sh
@@ -4,18 +4,36 @@ function install_process_rbenv() {
     local config_path=$1
     
     brew install rbenv ruby-build
+    
+    ###################################################################################
+    # Setup
+    #
+    # add rbenv init to shell
+    # FIXME 추후 shell type 에 따라서 수정할 설정 파일을 바꿔야할 듯..
+    echo 'if command -v rbenv 1>/dev/null 2>&1; then\n  eval "$(rbenv init -)"\nfi' >> ~/.bash_profile
+    # Restart shell
+    source ~/.bash_profile
+    ###################################################################################
 
     local versions=`cat $config_path | jq -r ".rbenv | .versions[].version"`
-    local global=`cat $config_path | jq -r ".rbenv | global"`
+    local global=`cat $config_path | jq -r ".rbenv | .global"`
 
     for v in $versions; do
+        echo "---------------------------------"
+        echo "rbenv install $v"
+        echo "---------------------------------"
         rbenv install $v
         rbenv local $v
         local packages=`cat $config_path | jq -r ".rbenv | .versions[] | select(.version==\"${v}\") | .packages[]"`
         for p in $packages; do
+            echo "---------------------------------"
+            echo "gem install $p"
+            echo "---------------------------------"
             gem install $p
         done
     done
     rbenv global $global
     rbenv local $global
 }
+
+# install_process_rbenv ../config.json
